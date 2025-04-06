9cbe534abccaa01f7b272e269967c2e5425e9f06
Gregory Casamento <greg.casamento@gmail.com>
2025-04-06 13:58:18 -0400

    Initial Commit

---
+++ PSOperatorsDrawing/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PSOperatorsDrawing
+//
+//  Created by Gregory John Casamento on 4/6/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ PSOperatorsDrawing/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  PSOperatorsDrawing
+//
+//  Created by Gregory John Casamento on 4/6/25.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
